388263daafb4732be353056933a1baf22a0a9358
Julien Hamaide <julien.hamaide@gmail.com>
2018-07-07 17:30:10 +0200

    Support for container in WM

--- src/ventus/WindowManager.hx
+++ src/ventus/WindowManager.hx
@@ -8,8 +8,14 @@ typedef CreateOptions = {
     @:optional var height: Int;
     @:optional var x: Int;
     @:optional var y: Int;
+    @:optional var content : String;
     @:optional var opacity : Float;
     @:optional var resizable : Bool;
+    @:optional var movable : Bool;
+    @:optional var widget : Bool;
+    @:optional var titlebar : Bool;
+    @:optional var animations : Bool;
+    @:optional var stayinspace : Bool;
 }
 
 @:native("Ventus.WindowManager")
@@ -17,12 +23,12 @@ extern class WindowManager {
 
     public var mode : String;
 
-    public function new();
+    public function new(?container:haxe.extern.EitherType<String, js.html.HtmlElement>);
 
     public function createWindow(options:CreateOptions) : Window;
 
 
-    inline public function createWindowFromQuery(query:String, options: CreateOptions) : Window{
+    inline public function createWindowFromQuery(query:String, options: CreateOptions) : Window {
         var _this = this;
         return untyped __js__("_this.createWindow.fromQuery")(query, options);
     }
